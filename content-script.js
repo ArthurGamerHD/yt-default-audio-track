d4063c4b2eb20d5a2296a5582d74960f70b91c1d
cheer <cheersupzoo@gmail.com>
2024-05-02 08:35:50 +0700

    fix: some initial case not trigger

--- content-script.js
+++ content-script.js
@@ -18,106 +18,88 @@ const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 let vid = getVideoEl();
 let lastTitle = "";
-let changed = false;
 
-if (vid && !vid.paused) {
+const changeLanguage = async () => {
   vid = getVideoEl();
+  if (!vid) {
+    return;
+  }
+  while (stopCounter < 250 && vid.readyState !== vid.HAVE_ENOUGH_DATA) {
+    await sleep(50);
+    stopCounter++;
+  }
+  if (stopCounter === 250) {
+    console.log("[YTDAT] Fail to detect Video");
+    return;
+  }
+  const {
+    audioTrackButtonTitle = "Audio track",
+    selectTrackTitle = "original",
+  } = await chrome.storage.sync.get([
+    "audioTrackButtonTitle",
+    "selectTrackTitle",
+  ]);
 
-  const changeLanguage = async () => {
-    if (changed) {
-      return;
-    }
-    changed = true
-    stopCounter = 0;
-    while (stopCounter < 250 && vid.readyState !== vid.HAVE_ENOUGH_DATA) {
-      await sleep(50);
-      stopCounter++;
-    }
-    if (stopCounter === 250) {
-      console.log("[YTDAT] Fail to detect Video");
-      return;
-    }
-    const {
-      audioTrackButtonTitle = "Audio track",
-      selectTrackTitle = "original",
-    } = await chrome.storage.sync.get([
-      "audioTrackButtonTitle",
-      "selectTrackTitle",
-    ]);
+  getSettingIconEl().click();
+  getSettingIconEl().click();
+  stopCounter = 0;
+  while (stopCounter < 250 && !getAudioTrackButton(audioTrackButtonTitle)) {
+    await sleep(50);
+    stopCounter++;
+  }
+  if (stopCounter === 250) {
+    console.log("[YTDAT] No Audio Track");
+    return;
+  }
 
-    getSettingIconEl().click();
-    getSettingIconEl().click();
-    stopCounter = 0;
-    while (stopCounter < 250 && !getAudioTrackButton(audioTrackButtonTitle)) {
-      await sleep(50);
-      stopCounter++;
-    }
-    if (stopCounter === 250) {
-      console.log("[YTDAT] No Audio Track");
-      return;
-    }
+  getAudioTrackButton(audioTrackButtonTitle).click();
 
-    getAudioTrackButton(audioTrackButtonTitle).click();
-
-    let nl = document.querySelectorAll(
-      "div.ytp-panel > div.ytp-panel-menu > div"
-    );
-
-    if (!nl.length) {
-      console.log("[YTDAT] No Audio Tracks found, skip!");
-      return;
-    }
-    let allEl = [...nl].map((el) => el.firstChild.firstChild.data);
-
-    let oriIndex = allEl.findIndex((text) =>
-      text.toLowerCase().includes(selectTrackTitle)
-    );
-
-    stopCounter = 0;
-    while (stopCounter < 200 && oriIndex === -1) {
-      await sleep(50);
-      allEl = [...nl].map((el) => el.firstChild.firstChild.data);
-      oriIndex = allEl.findIndex((text) =>
-        text.toLowerCase().includes(selectTrackTitle)
-      );
-      stopCounter++;
-    }
-
-    if (oriIndex === -1) {
-      console.log(
-        `[YTDAT] Audio Track include \x1B[94m${selectTrackTitle}\x1B[m not found!`
-      );
-      return;
-    }
-
-    nl[oriIndex]?.click();
-    console.info(`[YTDAT] Change Audio Track to \x1B[94m${allEl[oriIndex]}`);
-  };
-
-  window.addEventListener(
-    "yt-update-title",
-    (e) => {
-      if (e.detail?.length && e.detail !== lastTitle) {
-        changeLanguage();
-        lastTitle = e.detail;
-      }
-    },
-    true
+  let nl = document.querySelectorAll(
+    "div.ytp-panel > div.ytp-panel-menu > div"
   );
 
-  const observer = new MutationObserver(async (mutations, observer) => {
-    const tagNames = mutations.map((mutation) => mutation.target.tagName);
-    if (tagNames.includes("ytd-watch-flexy".toUpperCase())) {
-      changeLanguage();
-      observer.disconnect();
-    }
-  });
-  observer.observe(document, {
-    subtree: true,
-    childList: true,
-  });
-  if (document.querySelector("ytd-watch-flexy")) {
-    changeLanguage();
-    observer.disconnect();
+  if (!nl.length) {
+    console.log("[YTDAT] No Audio Tracks found, skip!");
+    return;
   }
+  let allEl = [...nl].map((el) => el.firstChild.firstChild.data);
+
+  let oriIndex = allEl.findIndex((text) =>
+    text.toLowerCase().includes(selectTrackTitle)
+  );
+
+  stopCounter = 0;
+  while (stopCounter < 200 && oriIndex === -1) {
+    await sleep(50);
+    allEl = [...nl].map((el) => el.firstChild.firstChild.data);
+    oriIndex = allEl.findIndex((text) =>
+      text.toLowerCase().includes(selectTrackTitle)
+    );
+    stopCounter++;
+  }
+
+  if (oriIndex === -1) {
+    console.log(
+      `[YTDAT] Audio Track include \x1B[94m${selectTrackTitle}\x1B[m not found!`
+    );
+    return;
+  }
+
+  if (nl[oriIndex].getAttribute("aria-checked") === "true") {
+    return;
+  }
+
+  nl[oriIndex]?.click();
+  console.info(`[YTDAT] Change Audio Track to \x1B[94m${allEl[oriIndex]}`);
+};
+
+window.addEventListener("yt-update-title", (e) => {
+  if (e.detail?.length && e.detail !== lastTitle) {
+    changeLanguage();
+    lastTitle = e.detail;
+  }
+});
+
+if (document.querySelector("ytd-watch-flexy")) {
+  changeLanguage();
 }
